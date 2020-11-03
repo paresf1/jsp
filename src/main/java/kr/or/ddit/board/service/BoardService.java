91677d0b6f6814f70a96e9eb9d4d8a69e1ce0bd7
lss <paresf4@gmail.com>
2020-11-03 09:18:12 +0900

    "스프링 설정  . component-scan  . java config"  . java config - componentScan"

--- src/main/java/kr/or/ddit/board/service/BoardService.java
+++ src/main/java/kr/or/ddit/board/service/BoardService.java
@@ -1,10 +1,18 @@
 package kr.or.ddit.board.service;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
 import kr.or.ddit.board.model.BoardVo;
 import kr.or.ddit.board.repository.BoardRepositoryI;
 
+// <bean id="boardService"/>
+@Service("boardService")
 public class BoardService implements BoardServiceI{
 	
+	//boardrepository의 것을 넣음
+	@Resource(name="boardRepository")
 	private BoardRepositoryI boardRepository;
 	
 	public BoardService() {
