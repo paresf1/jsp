d0ee1081cc119928bba5a4e13dfb7289c367b1ed
lss <paresf4@gmail.com>
2020-11-06 13:51:13 +0900

    "spring error page 설정  . mvc:resources"

--- src/main/java/kr/or/ddit/mvc/exception/NoFileException.java
+++ src/main/java/kr/or/ddit/mvc/exception/NoFileException.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 //사용자에게 500에러 대신 404응답 코드로 응답기 가게끔 설정
 
-@ResponseStatus(HttpStatus.NOT_FOUND)
+//@ResponseStatus(HttpStatus.NOT_FOUND)
 public class NoFileException extends Exception {
 	
 }
